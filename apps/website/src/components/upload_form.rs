ba9f308faaeb7eddaab0f8d68eeddd39f5346319
Aske Ching <aske@modulaer.io>
2024-12-17 09:14:45 +0100

    deploy website

--- apps/website/src/components/upload_form.rs
+++ apps/website/src/components/upload_form.rs
@@ -4,17 +4,8 @@ use leptos::prelude::*;
 pub fn UploadForm() -> impl IntoView {
     view! {
         <form>
-            <fieldset style="display: flex; flex-direction: column;">
-                <label>
-                    First
-                    <input name="first_name" placeholder="First name" autocomplete="given-name" />
-                </label>
-                <label>
-                    Email
-                    <input type="email" name="email" placeholder="Email" autocomplete="email" />
-                </label>
-                <label>Source code <input type="file" /></label>
-            </fieldset>
+            <input type="file" name="file" />
+            <button type="submit">Upload</button>
         </form>
     }
 }
