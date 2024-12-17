9a07426e3f7605ba746c5c31ada0402347964470
Aske Ching <aske@modulaer.io>
2024-12-17 21:54:00 +0100

    Dockerfile speedup attempt

--- apps/website/src/components/upload_form.rs
+++ apps/website/src/components/upload_form.rs
@@ -4,6 +4,7 @@ use leptos::prelude::*;
 pub fn UploadForm() -> impl IntoView {
     view! {
         <form>
+            <input type="text" name="name" />
             <input type="file" name="file" />
             <button type="submit">Upload</button>
         </form>
